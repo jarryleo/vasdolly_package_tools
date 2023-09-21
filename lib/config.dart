8f0326095b7b36dd22e55f736b92a0ba09141b9c
liujiarui <yjtx256@163.com>
2023-09-21 18:40:13 +0800

    [add] 圆角无边框

--- lib/config.dart
+++ lib/config.dart
@@ -1,16 +1,16 @@
-
 import 'package:get_storage/get_storage.dart';
 import 'package:window_manager/window_manager.dart';
 
 import 'includes.dart';
 
-class Config{
+class Config {
   ///初始化
   static Future<void> init() async {
     //初始化存储
     await GetStorage.init();
     await initWindow();
   }
+
   ///初始化窗口设置
   static Future<void> initWindow() async {
     WidgetsFlutterBinding.ensureInitialized();
@@ -22,10 +22,13 @@ class Config{
       minimumSize: Size(800, 600),
       center: true,
       skipTaskbar: false,
+      titleBarStyle: TitleBarStyle.hidden, // 注释：去除窗口标题栏
     );
     windowManager.waitUntilReadyToShow(windowOptions, () async {
+      await windowManager.setBackgroundColor(Colors.transparent);
       await windowManager.show();
       await windowManager.focus();
+      await windowManager.setAsFrameless();
     });
   }
 }
--- lib/page/main/main_page.dart
+++ lib/page/main/main_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_custom_tab_bar/library.dart';
 import 'package:vasdolly_package_tools/page/main/main_logic.dart';
 import 'package:vasdolly_package_tools/theme/colors.dart';
+import 'package:window_manager/window_manager.dart';
 
 import '../../includes.dart';
 
@@ -38,65 +39,84 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: QColors.bodyBg,
-      body: Row(
-        children: [
-          Column(
+      backgroundColor: Colors.transparent,
+      extendBody: true,
+      body: GestureDetector(
+        onPanStart: (e) {
+          windowManager.startDragging();
+        },
+        child: Container(
+          //圆角
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: QColors.bodyBg,
+          ),
+          child: Row(
             children: [
-              const SizedBox(height: 16),
-              const Text(
-                'PKG',
-                style: TextStyle(
-                  fontWeight: FontWeight.w800,
-                  fontSize: 50,
-                  color: QColors.mainColor,
-                ),
-              ),
-              const SizedBox(height: 20),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 8),
-                  child: CustomTabBar(
-                    width: 150,
-                    direction: Axis.vertical,
-                    pageController: mainLogic.pageController,
-                    tabBarController: mainLogic.tabBarController,
-                    itemCount: mainLogic.tabs.length,
-                    builder: getTabBarChild,
-                    indicator: RoundIndicator(
-                      color: QColors.contentBg,
-                      radius: const BorderRadius.only(
-                        topLeft: Radius.circular(8),
-                        bottomLeft: Radius.circular(8),
+              Column(
+                children: [
+                  const SizedBox(height: 16),
+                  const Text(
+                    'PKG',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w800,
+                      fontSize: 50,
+                      color: QColors.mainColor,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 8),
+                      child: CustomTabBar(
+                        width: 150,
+                        direction: Axis.vertical,
+                        pageController: mainLogic.pageController,
+                        tabBarController: mainLogic.tabBarController,
+                        itemCount: mainLogic.tabs.length,
+                        builder: getTabBarChild,
+                        indicator: RoundIndicator(
+                          color: QColors.contentBg,
+                          radius: const BorderRadius.only(
+                            topLeft: Radius.circular(8),
+                            bottomLeft: Radius.circular(8),
+                          ),
+                        ),
                       ),
                     ),
                   ),
+                  IconButton(
+                    onPressed: () {
+                      windowManager.close();
+                    },
+                    icon: const Icon(Icons.power_settings_new),
+                  ),
+                  const Text('by: Jarry Leo'),
+                  const SizedBox(height: 16),
+                ],
+              ),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.only(top: 32, right: 16, bottom: 16),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: QColors.contentBg,
+                  ),
+                  child: PageView.builder(
+                    allowImplicitScrolling: true,
+                    scrollDirection: Axis.vertical,
+                    physics: const NeverScrollableScrollPhysics(),
+                    controller: mainLogic.pageController,
+                    itemCount: mainLogic.pages.length,
+                    itemBuilder: (context, index) {
+                      return mainLogic.pages[index];
+                    },
+                  ),
                 ),
               ),
-              const Text('by: Jarry Leo'),
-              const SizedBox(height: 16),
             ],
           ),
-          Expanded(
-            child: Container(
-              margin: const EdgeInsets.only(top: 16, right: 16, bottom: 16),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: QColors.contentBg,
-              ),
-              child: PageView.builder(
-                allowImplicitScrolling: true,
-                scrollDirection: Axis.vertical,
-                physics: const NeverScrollableScrollPhysics(),
-                controller: mainLogic.pageController,
-                itemCount: mainLogic.pages.length,
-                itemBuilder: (context, index) {
-                  return mainLogic.pages[index];
-                },
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
